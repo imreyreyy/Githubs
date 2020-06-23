743dd1529b9d24fb0ab40746c27cf22675decd41
Reynolf <Reynolf@DESKTOP-FM22V42>
2020-06-23 11:31:21 +0800

    Create Gig Model

---
+++ GitHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace GitHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Gigs> Gigs { get; set; }
+
+        public DbSet<Genre> Genre { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
