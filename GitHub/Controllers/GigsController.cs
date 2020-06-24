203cc7b05ed15b544b03f08a636afc23744dc168
Reynolf <Reynolf@DESKTOP-FM22V42>
2020-06-24 20:48:26 +0800

    Add DropDown Menu - CSS

---
+++ GitHub/Controllers/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using GitHub.Models;
+using GitHub.ViewModel;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GitHub.Controllers
+{
+    public class GigsController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigsFormViewModel
+            {
+                Genres = _context.Genre.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ GitHub/ViewModel/GigsFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GitHub.Models;
+using System.Collections.Generic;
+
+namespace GitHub.ViewModel
+{
+    public class GigsFormViewModel
+    {
+
+        public string Venue { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        public int Genre { get; set; }
+
+
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
