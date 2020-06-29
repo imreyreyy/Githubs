859cbedf9dab33e5a4ffa00bb4ab70e4db22c281
Reynolf <Reynolf@DESKTOP-FM22V42>
2020-06-29 16:51:43 +0800

    Implement Edit

--- GitHub/Controllers/GigsController.cs
+++ GitHub/Controllers/GigsController.cs
@@ -57,16 +57,21 @@ namespace GitHub.Controllers
             return View("Gigs", viewModel);
 
         }
+
+        #region Create
         // GET: Gigs
         [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigsFormViewModel
             {
-                Genres = _context.Genre.ToList()
+                Genres = _context.Genre.ToList(),
+                Heading = "Add a Gig"
             };
-            return View(viewModel);
+            return View("GigForm", viewModel);
         }
+
+
         [HttpPost]
         [Authorize]
         [ValidateAntiForgeryToken]
@@ -76,22 +81,82 @@ namespace GitHub.Controllers
             {
                 //Genres need to populate first
                 viewModel.Genres = _context.Genre.ToList();
-                return View("Mine", "Gigs");
+                return View("GigForm", "Gigs");
             }
 
 
             var gigs = new Gigs
             {
                 ArtistId = User.Identity.GetUserId(),
-                DateTime =viewModel.GetDateTime(),
-                GenreId=viewModel.Genre,
-                Venue=viewModel.Venue
+                DateTime = viewModel.GetDateTime(),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
 
             };
             _context.Gigs.Add(gigs);
             _context.SaveChanges();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Home");
         }
+
+
+        #endregion
+
+
+        #region Edit
+
+
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+
+            var viewModel = new GigsFormViewModel
+            {
+                Heading = "Edit a Gig",
+                Id = gig.Id,
+                Genres = _context.Genre.ToList(),
+                Date = gig.DateTime.ToString("dd MMM yyyy "),
+                Time = gig.DateTime.ToString("HH:mm"),
+                Venue = gig.Venue,
+                Genre = gig.GenreId,
+            };
+            return View("GigForm", viewModel);
+        }
+
+
+
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(GigsFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                //Genres need to populate first
+                viewModel.Genres = _context.Genre.ToList();
+                return View("GigForm", viewModel);
+            }
+
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Gigs.Single(g => g.Id == viewModel.Id && g.ArtistId == userId);
+            gigs.Venue = viewModel.Venue;
+            gigs.DateTime = viewModel.GetDateTime();
+            gigs.GenreId = viewModel.Genre;
+
+            _context.SaveChanges();
+
+
+            return RedirectToAction("Mine", "Gigs");
+        }
+
+
+        #endregion
+
+
 
 
     }
--- GitHub/ViewModel/GigsFormViewModel.cs
+++ GitHub/ViewModel/GigsFormViewModel.cs
@@ -1,13 +1,16 @@
-﻿using GitHub.Models;
+﻿using GitHub.Controllers;
+using GitHub.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Web.Mvc;
 
 namespace GitHub.ViewModel
 {
     public class GigsFormViewModel
     {
-     
+        public int Id { get; set; }
         public DateTime GetDateTime()
         {
          return DateTime.Parse(string.Format("{0} {1}", Date, Time));
@@ -25,6 +28,25 @@ namespace GitHub.ViewModel
 
         [Required]
         public int Genre { get; set; }
+
+        public string Heading { get; set; }
+
+        public string Action
+        {
+            get
+            {
+                Expression<Func<GigsController, ActionResult>> update = 
+                    (c => c.Update(this));
+
+                Expression<Func<GigsController, ActionResult>> create = 
+                    (c => c.Create(this));
+
+              var action= (Id != 0) ? update : create;
+            
+
+                return ((action.Body as MethodCallExpression).Method.Name);
+            }
+                }
 
 
         public IEnumerable<Genre> Genres { get; set; }
